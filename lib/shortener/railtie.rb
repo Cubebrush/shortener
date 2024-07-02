7902a91df45290e68b75148bd071e2c248bb692b
cbmike <mike@cubebrush.co>
2024-07-02 19:05:17 +0300

    Rails 7 compatibility

--- lib/shortener/railtie.rb
+++ lib/shortener/railtie.rb
@@ -1,6 +1,7 @@
 module Shortener
   class Railtie < ::Rails::Railtie
     initializer "shortener.register.active.record.extension" do |app|
+      require_relative '../../app/middlewares/shortener_redirect_middleware'
       app.config.middleware.use ShortenerRedirectMiddleware
 
       ActiveSupport.on_load :active_record do
